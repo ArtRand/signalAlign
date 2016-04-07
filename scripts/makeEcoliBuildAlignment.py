5e6520ab3d0b6ae54d1563241437892a2a7a2716
ArtRand <arand@soe.ucsc.edu>
2016-04-06 22:05:57 -0700

    balanced kmer exposure for ecoli build alignments

--- scripts/makeEcoliBuildAlignment.py
+++ scripts/makeEcoliBuildAlignment.py
@@ -14,31 +14,36 @@ def parse_args():
     parser = ArgumentParser(description=__doc__)
 
     # query files
-    parser.add_argument('--alignments', '-a', action='store', dest='alns', required=True, type=str,
+    parser.add_argument('--alignments', '-a', action='append', dest='alns', required=True, type=str,
                         help="alignment files")
-    parser.add_argument('--labeled_alignments', '-la', action='append', dest='labeled', required=True, type=str,
+    parser.add_argument('--methylated', '-ma', action='append', dest='positive', required=True, type=str,
                         help="alignments to get labeled events from")
-    parser.add_argument('--number_of_assignments', '-n', action='store', type=int, default=10000,
+    parser.add_argument('--null', '-na', action='append', dest='null', required=True, type=str,
+                        help="alignments to get null events from")
+    parser.add_argument('--number_of_assignments', '-nb', action='store', type=int, default=10000,
                         dest='max_assignments',
-                        help='total number of assignments to collect FOR EACH GROUP')
+                        help='total number of canonical assignments to get')
     parser.add_argument('--number_of_labeled_assignments', '-l', action='store', type=int, default=10000,
-                        dest='max_labels', help='total number of assignments to collect FOR EACH GROUP')
+                        dest='max_labels', help='total number of assignments to collect for labeled and null')
     parser.add_argument('--threshold', '-t', action='store', type=float, default=0.75, dest='threshold')
-    parser.add_argument('--positions', '-p', action='store', dest='positions', required=True)
+    parser.add_argument('--label', action='store', default="E", dest='label')
+    parser.add_argument('--methylated_positions', '-m', action='store', dest='positive_positions', required=True)
+    parser.add_argument('--null_positions', '-n', action='store', dest='null_positions', required=True)
     parser.add_argument('--out', '-o', action='store', type=str, required=True, dest='out_file')
 
     return parser.parse_args()
 
 
 def parse_alignment_file(alignment_file):
-    data = pd.read_table(alignment_file, usecols=(1, 4, 9, 12, 13),
+    data = pd.read_table(alignment_file, usecols=(1, 4, 5, 9, 12, 13),
                          dtype={'ref_pos': np.int64,
                                 'strand': np.str,
+                                'event_index': np.int64,
                                 'kmer': np.str,
                                 'posterior_prob': np.float64,
                                 'event_mean': np.float64},
                          header=None,
-                         names=['ref_pos', 'strand', 'kmer', 'posterior_prob', 'event_mean'])
+                         names=['ref_pos', 'strand', 'event_index', 'kmer', 'posterior_prob', 'event_mean'])
     return data
 
 
@@ -59,10 +64,6 @@ def substitute_kmer(kmer, i, sub="E"):
     l[i] = sub
     return ''.join(l)
 
-#def cull_assignments(alignment_df, positions, strand):
-#    crit = data['ref_pos'].map(lambda x: x in positions)
-#    cytosine_hits = set(data[crit].ix[data['strand'] == 't']['ref_pos'])
-
 
 def get_reverse_complement_positions(positions):
     return [x - 3 for x in positions]
@@ -73,36 +74,38 @@ def get_hit_range(hit, kmer_length=6):
 
 
 def get_reverse_complement_hit_range(hit, kmer_length=6):
-    return range(hit , hit + kmer_length)
+    return range(hit, hit + kmer_length)
 
 
-def get_assignments(data, hit_range, strand, threshold=0.5):
+def get_assignments(data, hit_range, strand, substitution, threshold):
     assignments = []
     for i, h in enumerate(hit_range):
         crit = data['ref_pos'].map(lambda x: x == h)
         selected = data[crit].ix[(data['strand'] == strand) & (data['posterior_prob'] >= threshold)]
         kmers = selected['kmer'].tolist()
-        kmers = [substitute_kmer(x, i) for x in kmers]
+        kmers = [substitute_kmer(kmer=x, i=i, sub=substitution) for x in kmers]
         assignment = pd.DataFrame({"kmer": kmers,
                                    "event_mean": selected['event_mean'],
                                    "posterior_prob": selected['posterior_prob'],
                                    "ref_pos": selected['ref_pos'],
+                                   "event_index": selected['event_index'],
                                    "strand": strand})
         assignments.append(assignment)
     return pd.concat(assignments)
 
 
-def get_assignment_table(data, hits, strand, get_range_function, threshold):
+def get_assignment_table(data, hits, strand, get_range_function, substitution, threshold):
     assignment_table = []
     for hit in hits:
         hit_range = get_range_function(hit)
-        assignments = get_assignments(data, hit_range, strand, threshold)
+        assignments = get_assignments(data=data, hit_range=hit_range, strand=strand, substitution=substitution,
+                                      threshold=threshold)
         assignment_table.append(assignments)
     return assignment_table
 
 
-def get_labeled_assignmets_from_read(read, positions, forward, threshold, kmer_length=6):
-    data = parse_alignment_file(read)
+def get_labeled_assignmets_from_alignment(alignment, positions, forward, threshold, substitution, kmer_length):
+    data = parse_alignment_file(alignment)
     rc_positions = [x - 3 for x in positions]
 
     crit = data['ref_pos'].map(lambda p: p in positions)
@@ -112,19 +115,65 @@ def get_labeled_assignmets_from_read(read, positions, forward, threshold, kmer_l
     rc_hits = set(data[rc_crit].ix[data['strand'] == 't']['ref_pos'])
 
     if forward is True:
-        assignments = get_assignment_table(data, hits, 't', get_hit_range, threshold)
-        rc_assignments = get_assignment_table(data, rc_hits, 'c', get_reverse_complement_hit_range, threshold)
+        assignments = get_assignment_table(data=data, hits=hits, strand='t', get_range_function=get_hit_range,
+                                           substitution=substitution, threshold=threshold)
+        rc_assignments = get_assignment_table(data=data, hits=rc_hits, strand='c',
+                                              get_range_function=get_reverse_complement_hit_range,
+                                              substitution=substitution, threshold=threshold)
     else:
-        assignments = get_assignment_table(data, rc_hits, 't', get_reverse_complement_hit_range, threshold)
-        rc_assignments = get_assignment_table(data, hits, 'c', get_hit_range, threshold)
+        assignments = get_assignment_table(data=data, hits=rc_hits, strand='t',
+                                           get_range_function=get_reverse_complement_hit_range,
+                                           substitution=substitution, threshold=threshold)
+        rc_assignments = get_assignment_table(data=data, hits=hits, strand='c', get_range_function=get_hit_range,
+                                              substitution=substitution, threshold=threshold)
 
     return assignments + rc_assignments
 
 
-def randomly_select_alignments(path_to_alignments):
+def get_labeled_assignments(alignments, max_labels, positions, threshold, label, kmer_length=6):
+    labeled_assignments = []  # bin for DataFrames of assignments
+    total = 0  # count number of assignments we've gotten
+
+    for alignment in alignments:
+        if alignment.endswith(".backward.tsv"):
+            forward = False
+        else:
+            forward = True
+        # assignments is an array of DataFrames
+        assignments = get_labeled_assignmets_from_alignment(alignment=alignment, positions=positions,
+                                                            forward=forward, substitution=label,
+                                                            threshold=threshold, kmer_length=kmer_length)
+        assignments = [x for x in assignments if not x.empty]
+
+        new_assignment_total = sum([x.shape[0] for x in assignments])
+        print "Got {assignments} for {file}".format(assignments=new_assignment_total, file=alignment)
+
+        total += sum([x.shape[0] for x in assignments])
+
+        labeled_assignments += assignments
+        if total >= max_labels:
+            break
+
+    if total < max_labels:
+        print "Didn't get enough labels only got {notEnough} was aiming for {wanted} for label {label}" \
+              "".format(notEnough=total, wanted=max_labels, label=label)
+        return None
+
+    return labeled_assignments
+
+
+def get_alignments_from_directory(path_to_alignments):
     alignments = [x for x in glob.glob(path_to_alignments) if os.stat(x).st_size != 0]
     shuffle(alignments)
     return alignments
+
+
+def cull_list_of_alignment_files(list_of_directories):
+    list_of_alignments = []
+    for directory in list_of_directories:
+        list_of_alignments += get_alignments_from_directory(directory)
+    shuffle(list_of_alignments)
+    return list_of_alignments
 
 
 def collect_assignments(alignments, threshold, max_assignments, positions):
@@ -158,44 +207,43 @@ def collect_assignments(alignments, threshold, max_assignments, positions):
 
 def main(args):
     args = parse_args()
-    # randomly collect some alignments, and remove the events that correspont to methylated positions
-    alignments = randomly_select_alignments(args.alns)
-    f, b = parse_substitution_file(args.positions)
-    forward_sub, forward_pos = f[0], f[1]
+    # wrangle the positions we're calling positive and null
+    pf, pb = parse_substitution_file(args.positive_positions)
+    f_positive_substitution, forward_positive_sites = pf[0], pf[1]
+    nf, nb = parse_substitution_file(args.null_positions)
+    f_null_substitution, forward_null_sites = nf[0], nf[1]
+
+    # randomly collect some alignments, and remove the events that correspond to positions we're going to label
+    alignments = cull_list_of_alignment_files(args.alns)
     print "Getting canonical assignments"
-    canonical_assignments = collect_assignments(alignments,
-                                                args.threshold,
-                                                args.max_assignments,
-                                                forward_pos)
+    canonical_assignments = collect_assignments(alignments=alignments,
+                                                threshold=args.threshold,
+                                                max_assignments=args.max_assignments,
+                                                positions=(forward_positive_sites + forward_null_sites))
+
     print "Got {} canonical assignments".format(canonical_assignments.shape[0])
-    targeted_alignments = []
-    for target_alignment in args.labeled:
-        targeted_alignments += randomly_select_alignments(target_alignment)
-        shuffle(targeted_alignments)
 
     print "Getting labeled assignments"
-    labeled_assignments = []
-    total = 0
-    while total < args.max_labels:
-        for alignment in targeted_alignments:
-            if alignment.endswith(".backward.tsv"):
-                forward = False
-            else:
-                forward = True
-            assignments = get_labeled_assignmets_from_read(alignment, forward_pos, forward, args.threshold)
-            new_assignment_total = sum([x.shape[0] for x in assignments])
-            print "Got {assignments} for {file}".format(assignments=new_assignment_total, file=alignment)
-            total += sum([x.shape[0] for x in assignments])
-            assignments = [x for x in assignments if not x.empty]
-            labeled_assignments += assignments
-            if total >= args.max_labels:
-                break
-        if total < args.max_labels:
-            print "Didn't get enough labels only got {notEnough} was aiming for {wanted}, exiting" \
-                  "".format(notEnough=total, wanted=args.max_labels)
-            sys.exit(1)
+    # get the list of alignments we're going to label
+    positive_alignments = cull_list_of_alignment_files(args.positive)
+    positive_assignments = get_labeled_assignments(alignments=positive_alignments, max_labels=args.max_labels,
+                                                   positions=forward_positive_sites, threshold=args.threshold,
+                                                   label=args.label)
+    if positive_assignments is None:
+        sys.exit(1)
 
-    print "Got {} labeled assignments".format(total)
+    print "Getting null assignments"
+    null_alignments = cull_list_of_alignment_files(args.null)
+    null_assignments = get_labeled_assignments(alignments=null_alignments, max_labels=args.max_labels,
+                                               positions=forward_null_sites, threshold=args.threshold,
+                                               label="C")
+    if null_assignments is None:
+        sys.exit(1)
+
+    total_labeled = sum([x.shape[0] for x in positive_assignments])
+    total_null = sum([x.shape[0] for x in null_assignments])
+    print "Got {labeled} labeled assignments and {null} null assignments".format(labeled=total_labeled,
+                                                                                 null=total_null)
 
     entry_line = "blank\t0\tblank\tblank\t{strand}\t0\t0.0\t0.0\t0.0\t{kmer}\t0.0\t0.0\t0.0\t{event}\t0.0\n"
 
@@ -205,9 +253,13 @@ def main(args):
         for row in canonical_assignments.itertuples():
             f.write(entry_line.format(strand=row[5], kmer=row[2], event=row[1]))
 
-        for frame in labeled_assignments:
-            for row in frame.itertuples():
-                f.write(entry_line.format(strand=row[5], kmer=row[2], event=row[1]))
+        for dataframe in positive_assignments:
+            for row in dataframe.itertuples():
+                f.write(entry_line.format(strand=row[6], kmer=row[3], event=row[2]))
+
+        for dataframe in null_assignments:
+            for row in dataframe.itertuples():
+                f.write(entry_line.format(strand=row[6], kmer=row[3], event=row[2]))
 
     print "DONE"
 
