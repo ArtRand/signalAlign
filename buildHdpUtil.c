5fa8a2eb611f0890103ac8b0541992aeb72b970d
ArtRand <arand@soe.ucsc.edu>
2017-01-28 19:58:56 -0800

    fixed hdptype check

--- buildHdpUtil.c
+++ buildHdpUtil.c
@@ -246,8 +246,8 @@ int main(int argc, char *argv[]) {
                                                   samplingGridStart, samplingGridEnd, samplingGridLength);
             
     } else {
-        if (hdpType != singleLevelPrior2 || hdpType != multisetPrior2) {
-            st_errAbort("Invalid HDP type for 1D %s", hdpType);
+        if (hdpType != singleLevelPrior2 && hdpType != multisetPrior2) {
+            st_errAbort("Invalid HDP type for 1D %i", hdpType);
         }
         NanoporeHdpType type = (NanoporeHdpType) hdpType;
         nanoporeHdp_buildOneDHdpFromAlignment(type, kmerLength,
